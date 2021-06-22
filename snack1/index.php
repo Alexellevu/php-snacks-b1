2d05ffb918e13c54a6df9f9b6330892f72fb0fe5
alexellevu <newnapster83@gmail.com>
2021-06-22 16:31:08 +0200

    struttura snack 1

--- snack1/index.php
+++ snack1/index.php
@@ -1,7 +1,32 @@
 <!-- Snack 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
+<?php
+$calendario=[
+$tappa1 = [
+    'squadra_casa' => 'Olimpia Milano',
+    'squadra_ospite' => 'Cantù',
+    'Punti_sq_casa' => '55',
+    'Punti_sq_ospite' => '60'
+],
 
+$tappa2 = [
+    'squadra_casa' => 'Virtus Bologna',
+    'squadra_ospite' => 'Venezia',
+    'Punti_sq_casa' => '50',
+    'Punti_sq_ospite' => '45'
+],
+
+$tappa3 = [
+    'squadra_casa' => 'Dinamo Sassari',
+    'squadra_ospite' => 'Trento',
+    'Punti_sq_casa' => '70',
+    'Punti_sq_ospite' => '65'
+]
+
+];
+
+?>
 
 
 
@@ -14,6 +39,10 @@ Olimpia Milano - Cantù | 55-60 -->
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Calendario incontri serie A Basket</h1>
+    <?php  
+    for($i =0; $i < count($calendario) ;$i++){?>
+        <p> <?php echo $tappa[$i]['squadra_casa'] ?></p>
+    <?php } ?>
 </body>
 </html>
